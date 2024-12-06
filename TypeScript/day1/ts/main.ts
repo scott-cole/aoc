1cb1a9e5e7a0850ac387a04d37b1eb916a66b6e6
Scott Cole <scott.cole@rockstarleeds.com>
2024-12-06 08:31:38 +0000

    [DAY1-TS] find distances

--- TypeScript/day1/ts/main.ts
+++ TypeScript/day1/ts/main.ts
@@ -1,2 +1,40 @@
 const text = await Deno.readTextFile("input.txt")
-console.log(text.length);
+
+// Split List into 2 Arrays
+const listA: number[] = [];
+const listB: number[] = [];
+
+const splitLines = text.split("\n");
+
+splitLines.forEach(line => {
+  const numbers = line.split(" ");
+  const numberA = parseInt(numbers[0]);
+  const numberB = parseInt(numbers[numbers.length - 1]);
+  
+  listA.push(numberA);
+  listB.push(numberB);
+})
+
+// Sort Lists into order
+const sortedListA = listA.sort();
+const sortedListB = listB.sort();
+
+const distances: number[] = [];
+
+// Find lowest number listA and use index to index listB
+sortedListA.forEach((aNum, i) => {
+  const bNum = sortedListB[i];
+  const distance = Math.abs(aNum - bNum);
+  
+  // Store the answer in new list
+  distances.push(distance)
+})
+
+// add difference totals to get answer
+let totalDistance = 0
+
+distances.forEach((num) => {
+  totalDistance += num 
+})
+
+console.log(totalDistance)
