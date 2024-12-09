8e5b0982459258bbec17a3733d5059f87ce75849
Scott Cole <mcscott01@hotmail.com>
2024-12-09 12:10:05 +0000

    day2 correct

--- TypeScript/day2/ts/main.ts
+++ TypeScript/day2/ts/main.ts
@@ -1,49 +1,49 @@
 const increasing = (report: number[]) => {
   for (let i = 0; i < report.length - 1; i++) {
-    if ( report[i] < report[i + 1]) {
+    if (report[i] > report[i + 1]) {
       return false;
     }
   }
-  
+
   return true;
 };
 
 const decreasing = (report: number[]) => {
   for (let i = 0; i < report.length - 1; i++) {
-    if ( report[i] < report[i + 1]) {
+    if (report[i] < report[i + 1]) {
       return false;
     }
   }
-  
+
   return true;
 };
 
-const oneway = (report: number[]) => {
-  return increasing(report) || decreasing(report)
+const oneDirection = (report: number[]) => {
+  return increasing(report) || decreasing(report);
 };
 
-const text = await Deno.readTextFile("input.txt")
-const splitLines = text.split("\n").slice(0, -1);
+const text = await Deno.readTextFile("input.txt");
+const lines = text.split("\n");
 
-const reports = splitLines
-.map((line) => {
-  const elems = line.split(" ");
-  return elems.map((elem) => parseInt(elem))
-})
-.filter(report => oneway(report))
-.map((report) => {
-  return report.sort((i, j) => i - j);
-});
+const reports = lines
+  .map((line) => {
+    const elems = line.split(" ");
+    return elems.map((elem) => parseInt(elem));
+  })
+  .filter((report) => oneDirection(report))
+  .map((report) => {
+    return report.sort((i, j) => i - j);
+  });
 
 let sum = 0;
 
-reports.forEach(report => {
-  for (let i = 0; i < reports.length - 1; i++){
+reports.forEach((report) => {
+  for (let i = 0; i < reports.length - 1; i++) {
     const diff = report[i + 1] - report[i];
 
-   if (diff <= 0 || diff > 3) {
-    return;
-   }
+    if (diff <= 0 || diff > 3) {
+      return;
+    }
   }
 
   sum += 1;
